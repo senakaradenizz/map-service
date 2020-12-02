e6aaaf6fe04434e54eb2bb75513e8b959beb19a7
senakaradenizz <senaakaradenizz@gmail.com>
2020-12-02 15:19:42 +0300

    repositories added

---
+++ map-service/src/main/java/com/example/mapservice/repository/CountryRepository.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.example.mapservice.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.example.mapservice.model.Country;
+import org.springframework.stereotype.Repository;
+
+/**
+ *
+ * @author sena
+ */
+@Repository
+public interface CountryRepository extends JpaRepository<Country, Long>{
+    
+}
---
+++ map-service/src/main/java/com/example/mapservice/repository/SubprovinceRepository.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.example.mapservice.repository;
+
+import com.example.mapservice.model.Subprovince;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+/**
+ *
+ * @author sena
+ */
+
+@Repository
+public interface SubprovinceRepository extends JpaRepository<Subprovince, Long>{
+    
+}
