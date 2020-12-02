c42d2a14c25094e2c121ac5b53414fbcd40bcaba
Mehmet <mehmet.kapson@proby.intelprobe.com.tr>
2020-12-02 16:20:08 +0300

    Controller and validation dependency added

---
+++ map-service/src/main/java/com/example/mapservice/controller/MapServiceController.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.example.mapservice.controller;
+
+import com.example.mapservice.repository.CoordinateRepository;
+import com.example.mapservice.repository.CountryRepository;
+import com.example.mapservice.repository.ProvinceRepository;
+import com.example.mapservice.repository.SubprovinceRepository;
+import com.example.mapservice.model.Country;
+import java.util.List;
+import javax.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping("/api")
+public class MapServiceController {
+    
+    @Autowired
+    CountryRepository countryRepository;
+    @Autowired
+    ProvinceRepository provinceRepository;
+    @Autowired
+    SubprovinceRepository subprovinceRepository;
+    @Autowired
+    CoordinateRepository coordinateRepository;
+    
+     
+}
